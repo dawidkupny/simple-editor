56c250e0a1071d1361ca3e4a183d1fde91fc5b3d
Dawid <davvidkupny@gmail.com>
2021-02-19 10:50:52 +0100

    Made primary simple editor functionalities

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,22 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('Welcome to simple editor!')
+
+
+const notesInput = document.querySelector('.text--js');
+const loadButton = document.querySelector('.load--js');
+const saveButton = document.querySelector('.save--js');
+const clearButton = document.querySelector('.clear--js');
+
+saveButton.addEventListener("click", () => {
+    localStorage.setItem('notes', notesInput.value);
+});
+
+loadButton.addEventListener("click", () => {
+    document.querySelector('.text--js').innerHTML = localStorage.getItem('notes');
+});
+
+clearButton.addEventListener("click", () => {
+    localStorage.removeItem('notes');
+});
