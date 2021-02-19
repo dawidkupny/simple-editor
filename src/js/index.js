90e52994ff07f23040cd3dffde2a18d6de310e98
Dawid <davvidkupny@gmail.com>
2021-02-19 11:01:49 +0100

    Added messaging functionality

--- src/js/index.js
+++ src/js/index.js
@@ -13,15 +13,25 @@ const notesInput = document.querySelector('.text--js');
 const loadButton = document.querySelector('.load--js');
 const saveButton = document.querySelector('.save--js');
 const clearButton = document.querySelector('.clear--js');
+const infoOutput = document.querySelector('.info--js');
 
 saveButton.addEventListener("click", () => {
     localStorage.setItem('notes', notesInput.value);
+    infoOutput.innerHTML = "Zapisano notatkę!";
 });
 
 loadButton.addEventListener("click", () => {
-    document.querySelector('.text--js').innerHTML = localStorage.getItem('notes');
+    const notes = localStorage.getItem('notes');
+    document.querySelector('.text--js').innerHTML = notes;
+    if(notes) {
+        infoOutput.innerHTML = "Wczytano notatkę!";
+    }
+    else {
+        infoOutput.innerHTML = "Brak zapisanej notatki!";
+    }
 });
 
 clearButton.addEventListener("click", () => {
     localStorage.removeItem('notes');
+    infoOutput.innerHTML = "Usunięto zapisaną notatkę!";
 });
